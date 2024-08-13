fe8e50f74999ae6eb52a96c100504bcdcdea59e6
MarcosDelhugo <marcos.delhugo@mi.unc.edu.ar>
2024-08-13 14:59:16 -0300

    Falta Logica

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,6 +5,8 @@ import { IoMdSearch } from "react-icons/io";
 import Button from '@mui/material/Button';
 import { FiUser } from "react-icons/fi";
 import { IoBagHandleOutline } from "react-icons/io5";
+import "./index.css"
+
 const Header = () => {
     return (<>
         <div className="headerWrapper">
@@ -14,33 +16,25 @@ const Header = () => {
                 </div>
             </div>
             <header className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="logoWrapper d-flex align-items-center col-sm-2">
-                            <Link to={"/"}><img src={Logo} alt='Logo'/></Link>
-                        </div>
-
-                        <div className="col-sm-10 d-flex align-items-center part2">
-                            <CountryDropDown></CountryDropDown>
-                            {/*header search start here */}
-                            <div className="headerSearch ml-1 mr-1">
-                                <input type='text' placeholder='Search for products...'></input>
-                                <Button><IoMdSearch/></Button>
-                            </div>
-                            {/*header search end here */}
-                            
-                            <div className='part3 d-flex align-items-center ml-auto'>
-                                <Button className='circle mr-3'><FiUser/></Button>
-                                <div className='ml-auto cartTab d-flex aling-items-center'>
-                                    <span className='price'>$3.29</span>
-                                    <div className='position-relative ml-2'>
-                                        <Button className='circle'><IoBagHandleOutline/></Button>
-                                        <span className='count d-flex aling-items-center justify-content-center'>
-                                           1 
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="logoWrapper">
+                    <Link to={"/"}><img src={Logo} alt='Logo'/></Link>
+                </div>
+                <CountryDropDown></CountryDropDown>
+                <div className="col-sm-10  part2">
+                    {/*header search start here */}
+                    <div className="headerSearch ml-1 mr-1">
+                        <input type='text' placeholder='Search for products...'></input>
+                        <Button><IoMdSearch/></Button>
+                    </div>
+                    {/*header search end here */}
+                </div>
+                <Button className='userHeader mr-3'><FiUser/></Button>
+                <span className='price'>$3.29</span>
+                <div className='part3'>
+                    <div className='ml-auto cartTab d-flex aling-items-center'>
+                        <div className='position-relative ml-2'>
+                            <Button className='bagHeader'><IoBagHandleOutline/></Button>
+                            <span className='count d-flex aling-items-center justify-content-center'>2</span>
                         </div>
                     </div>
                 </div>
